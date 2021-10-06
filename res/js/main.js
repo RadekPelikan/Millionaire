386115755cfb9819d9e5a9473f1ba3413dc21ef9
Radek Pelikan <pelikan.radek03@gmail.com>
2021-10-06 19:22:12 +0200

    functioning buttons and layout

--- res/js/main.js
+++ res/js/main.js
@@ -1,7 +1,6 @@
-const initSlide = document.getElementById("next-level");
 const levelBlock = document.getElementById("level-wrapper");
 const questionText = document.getElementById("question");
-const buttonsBLock = document.getElementById("button-wrapper");
+const buttonsBlock = document.getElementById("button-wrapper");
 
 // TODO: Multiple correct answers
 let answerButtons;
@@ -13,7 +12,7 @@ let hasAnswered = false;
 const readFile = async () => {
     try {
         const file = await fetch("res/data/data.json");
-        console.log("Json load successful")
+        await console.log("Json load successful");
         return await file.json();
     } catch (e) {
         return readFileGitHub();
@@ -47,24 +46,25 @@ const HandleNextQuestion = async () => {
         hasAnswered = false;
     }, 2100);
 };
-initSlide.onclick = () => {
-    currentQ = 0;
-    HandleNextQuestion();
-};
 
 const generateButtons = async () => {
     answerButtons = [];
-    buttonsBLock.innerHTML = "";
-    await questionsJson[currentQ]["answers"].forEach((text, index) => createButton(text, index));
-}
+    buttonsBlock.innerHTML = "";
+    await questionsJson[currentQ]["answers"].forEach((text, index) =>
+        createButton(text, index)
+    );
+};
 
 const createButton = (answerText, index) => {
     let button = document.createElement("button");
+
     button.dataset.id = index;
     button.onclick = checkAnswer;
-    buttonsBLock.appendChild(button);
     button.innerText = answerText;
-    button.classList.add("button");
+
+    button.classList.add("button","is-fullwidth", "is-large", "my-3");
+
+    buttonsBlock.appendChild(button);
     answerButtons.push(button);
 };
 
@@ -75,15 +75,15 @@ const checkAnswer = () => {
     if (button.dataset.id == parseInt(questionsJson[currentQ].correctAns) - 1) {
         button.classList.add("is-success");
     } else {
-        button.classList.add("is-danger")
+        button.classList.add("is-danger");
     }
     hasAnswered = true;
 
     setTimeout(() => {
         HandleNextQuestion();
         currentQ++;
-    }, 1000)
-}
+    }, 1000);
+};
 
 window.onload = async () => {
     questionsJson = await readFile();
